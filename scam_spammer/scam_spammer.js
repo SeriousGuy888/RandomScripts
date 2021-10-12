42c8979b99ff169ce29551017b4eed164da1f85e
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-10-12 19:19:43 -0400

    a different scam this time

--- scam_spammer/scam_spammer.js
+++ scam_spammer/scam_spammer.js
@@ -2,17 +2,17 @@ import fetch from "node-fetch"
 import faker from "faker"
 
 
-const url = "https://steamsdiscord.com/login/dologin"
+const url = "https://discordrgift.com/discord/login"
 
 const randBool = () => !!Math.round(Math.random())
 const randInt = (max) => Math.floor(Math.random() * max)
 
 const sendReq = async () => {
-  let username = faker.internet.userName().replace(/\./g, "")
+  // let username = faker.internet.userName().replace(/\./g, "")
+  let email = faker.internet.email()
   let password
   let userAgent = faker.internet.userAgent()
-  // let referrer = "https://steamsdiscord.com/zbd4a5a8d2q0lLPv6lC6PKnQCqV4Vi6AITXzlz79FYuWMQV4NdY4HutuA"
-  let referrer = `https://steamsdiscord.com/zbd4a5a8d2${faker.random.alphaNumeric(47)}`
+  let referrer = `https://discordrgift.com/FGk22Djqo54s`
 
 
 
@@ -27,22 +27,25 @@ const sendReq = async () => {
     "credentials": "include",
     "headers": {
       "User-Agent": userAgent,
-      "Accept": "*/*",
+      "Accept": "application/json, text/plain, */*",
       "Accept-Language": "en-CA,en-US;q=0.7,en;q=0.3",
-      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-      "X-Requested-With": "XMLHttpRequest",
+      "Content-Type": "application/json",
       "Sec-Fetch-Dest": "empty",
       "Sec-Fetch-Mode": "cors",
-      "Sec-Fetch-Site": "same-origin"
+      "Sec-Fetch-Site": "same-origin",
+      "Sec-GPC": "1"
     },
     "referrer": referrer,
-    "body": `username=${username}&password=${password}`,
+    "body": JSON.stringify({
+      login: email,
+      password,
+    }),
     "method": "POST",
     "mode": "cors"
   })
 
-  console.log(`---\n${username} : ${password}\n${userAgent}\n${referrer}`)
-  console.log(await res.json())
+  console.log(`---\n${email} : ${password}\n${userAgent}\n${referrer}`)
+  console.log(res.status)
 }
 
 setInterval(sendReq, 500)
